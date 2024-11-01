eb33d45e43c1d1044556d723936720332f0887db
zwowfwl <zlata.kupreichyk@gmail.com>
2024-11-01 18:25:02 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": ";
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
-return 0;
+	int k, i = 1, sum_k = 0, number;
+// Asking the user to enter the amount of integers to sum
+	cout << " Enter the amount of integers to sum = "; cin >> k;
+// Stop the program if the amount equals to zero
+	if (k == 0) {
+		cout << "No integers for addition" << endl;
+	}
+// Calculating the sum if the amount is non-zero
+	else {
+		do {
+			cout << "Enter integer nr. " << i << ": ";
+			cin >> number;
+			sum_k += number; i++;
+		} while (i <= k);
+		cout << " The total sum of " << k << " integers is: " << sum_k;
+	}
+	return 0;
 }
